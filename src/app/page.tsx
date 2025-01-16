68c8573e9e989a85658e6f5e83e757ecf68c0e06
keitqr <119758473+keitqr@users.noreply.github.com>
2025-01-16 20:21:27 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 // app/page.tsx
 'use client';
 
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Head from 'next/head'; // Εισαγωγή του Head component
-import Image from 'next/image'; // Εισαγωγή του Image component
+import Head from 'next/head';
 
 const Home = () => {
   const router = useRouter();
@@ -16,11 +14,9 @@ const Home = () => {
   return (
     <>
       <Head>
-        {/* Εδώ μπορείς να προσθέσεις τα στοιχεία του head */}
         <title>Η Πρόσκληση για Ανατολή</title>
         <meta name="description" content="Πρόσκληση για να δεις την ανατολή του ηλίου μαζί!" />
       </Head>
-
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-900 to-purple-900 p-6 relative">
         <div className="text-center">
           <h1 className="text-4xl font-bold text-white mb-14">Καλως όρισες δεινόσαυρε TΥΧΑΙΑ στο παιχνίδι αυτό.</h1>
@@ -32,14 +28,12 @@ const Home = () => {
             Ξεκίνα το παιχνίδι
           </button>
         </div>
-
-        {/* Εικόνα Δεινοσαύρου κάτω δεξιά με διπλάσιο μέγεθος */}
         <Image 
           src="/dinosaur.png" 
           alt="Ένας πολύχρωμος δεινόσαυρος σε κινούμενη πόζα" 
           className="absolute bottom-4 right-4 w-64 h-64"
-          width={256} 
-          height={256} 
+          width={256}
+          height={256}
         />
       </div>
     </>
