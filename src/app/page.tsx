bc08bf4ff5bb3751b12dd53478a356eb61b96471
keitqr <119758473+keitqr@users.noreply.github.com>
2025-01-17 18:39:16 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation';
 import Head from 'next/head';
 import Image from 'next/image';
 
-
 const Home = () => {
   const router = useRouter();
 
@@ -21,11 +20,15 @@ const Home = () => {
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-900 to-purple-900 p-6 relative">
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-white mb-14">Καλως όρισες δεινόσαυρε TΥΧΑΙΑ στο παιχνίδι αυτό.</h1>
-          <p className="text-xl mb-20 text-white mb-16">Ξεκίνησε την περιπέτειά σου λύνοντας τους γρίφους και θα φτάσεις σε μία σπέσιαλ πρόσκληση!</p>
+          <h1 className="text-3xl sm:text-4xl font-bold text-white mb-8 sm:mb-14">
+            Καλως όρισες δεινόσαυρε TΥΧΑΙΑ στο παιχνίδι αυτό.
+          </h1>
+          <p className="text-lg sm:text-xl text-white mb-12 sm:mb-16">
+            Ξεκίνησε την περιπέτειά σου λύνοντας τους γρίφους και θα φτάσεις σε μία σπέσιαλ πρόσκληση!
+          </p>
           <button
             onClick={startQuiz}
-            className="px-8 py-4 bg-cyan-500 text-white rounded shadow-lg hover:bg-green-600 transform transition-all duration-300"
+            className="px-6 sm:px-8 py-3 sm:py-4 bg-cyan-500 text-white rounded shadow-lg hover:bg-green-600 transform transition-all duration-300"
           >
             Ξεκίνα το παιχνίδι
           </button>
@@ -33,7 +36,7 @@ const Home = () => {
         <Image 
           src="/dinosaur.png" 
           alt="Ένας πολύχρωμος δεινόσαυρος σε κινούμενη πόζα" 
-          className="absolute bottom-4 right-4 w-64 h-64"
+          className="absolute bottom-4 right-4 w-40 h-40 sm:w-64 sm:h-64"
           width={256}
           height={256}
         />
